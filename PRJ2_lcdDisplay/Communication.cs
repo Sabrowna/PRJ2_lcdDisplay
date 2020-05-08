0d4497ef79d0730812b170ec3c9c6f17c01b29c4
Danny Bøjstrup Pedersen <20031954@post.au.dk>
2020-05-08 11:53:23 +0200

    Battery.cs tilføjet. Metoder oprettet i klassen, og interaktion med PresentationLayer (Communication.cs) oprettet. Forklaringer på mine tanker om hvad der burde ske tilføjet som udkommenteret kode.

--- PRJ2_lcdDisplay/Communication.cs
+++ PRJ2_lcdDisplay/Communication.cs
@@ -15,7 +15,9 @@ namespace PresentationLayer
         private TWIST twist;
         private Display displayRef;
         private Ekg_Record ekgRecordRef;
-
+        private Battery batteryRef;
+        public bool isCharging = false; // Værdi skal hentes fra det fysiske system. Knap, kontakt etc.
+        // der bør også i forhold til isCharging laves en binding der tjekker om oplader pludselig bliver tilsluttet.
 
         public Communication()
         {
@@ -23,12 +25,41 @@ namespace PresentationLayer
             twist = new TWIST();
             ekgRecordRef = new Ekg_Record();
             displayRef = new Display();
+            batteryRef = new Battery();
         }
         public void Program()
         {
             
             lcd.lcdDisplay(); //Tænder skærmen
+            lcd.lcdClear(); // Nulstiller skærm
+            batteryRef.ShowBatteryStatus(); // Tjekker status på batteri
+
+            lcd.lcdGotoXY(0, 1);
+            lcd.lcdPrint($" Batteristatus: {batteryRef.ShowBatteryStatus()}"); // Udskriver batteristatus på display
+
+            Thread.Sleep(3000); // Venter i 3 sek. så det er muligt at se status på batteri både på display LED
+
+            if (batteryRef.ShowBatteryStatus() < 20) // Hvis batteristatus er lav jf. UC, udskrives nedenstående
+            {
+                while (batteryRef.ShowBatteryStatus() < 20) // Så længe batteristatus er lav, bliver systemet i denn løkke
+                {
+                    lcd.lcdGotoXY(0, 0);
+                    lcd.lcdPrint("Enhed deaktiveret   Batteristatus lav   Tilslut oplader");
+                }
+
+            }
+
+            if (batteryRef.Charging(isCharging) == true) // Hvis opladning er i gang, køres og udskrives som nedenfor
+            {
+                while (batteryRef.Charging(isCharging) == true) // Så længe opladning er i gang, bliver systemet i denne løkke
+                {
+                    lcd.lcd.lcdGotoXY(0, 1);
+                    lcd.lcdPrint("Opladning i gang    Måling ikke mulig");
+                }
+            }
+
             lcd.lcdClear();
+
 
             displayRef.GetEmployeeId(); //Medarbejderen logger ind
 
